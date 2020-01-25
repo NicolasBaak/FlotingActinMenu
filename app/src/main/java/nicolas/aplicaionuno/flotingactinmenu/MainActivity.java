67de5b678932070f54a886be493a63b9b09568c1
Nicolas Baak <nicolas123alejandro@gmail.com>
2020-01-25 10:25:18 -0600

    Initial commit

---
+++ app/src/main/java/nicolas/aplicaionuno/flotingactinmenu/MainActivity.java
@@ -0,0 +1,43 @@
+package nicolas.aplicaionuno.flotingactinmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.action1);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            showToat("fan 1");
+            }
+        });
+
+
+
+        FloatingActionButton floatingActionButton2 = findViewById(R.id.action2);
+
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToat("fan 2");
+            }
+        });
+    }
+
+    public void showToat(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
